1bde326e7952ac8fb3e60e7ac2ac76cf2c1373e0
Albert Gultom <gultomic@gmail.com>
2024-04-23 10:51:41 +0700

    update 23apr #1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,6 +29,25 @@
                 <div class="mt-16">
                     <livewire:event-public />
                 </div>
+
+                <div class="my-16">
+                    <p class="py-4 text-2xl font-semibold tracking-wide text-center sm:text-justify">Layanan Kami</p>
+
+                    <div class="grid grid-cols-1 gap-4 lg:grid-cols-3 justify-stretch">
+                        <a href="https://paskerid.kemnaker.go.id/layanan-detail/4273f2f2-ded5-4710-9931-31a78adf0112" class="py-8 border rounded-md hover:border-gray-400 group">
+                            <img class="h-12 mx-auto" src="https://paskerid.kemnaker.go.id/storage/4273f2f2-ded5-4710-9931-31a78adf0112_Logo SIAPkerja.png" alt="siapkerja">
+                            <p class="pt-4 text-center group-hover:text-blue-600">SIAPKerja</p>
+                        </a>
+                        <a href="https://paskerid.kemnaker.go.id/layanan-detail/ea93475e-54d2-4358-b295-9a72d4c77015" class="py-8 border rounded-md hover:border-gray-400 group">
+                            <img class="h-12 mx-auto" src="https://paskerid.kemnaker.go.id/storage/ea93475e-54d2-4358-b295-9a72d4c77015_Logo Karirhub.png" alt="siapkerja">
+                            <p class="pt-4 text-center group-hover:text-blue-600">Karirhub</p>
+                        </a>
+                        <a href="https://paskerid.kemnaker.go.id/layanan-detail/173e7033-8219-4ddf-809c-ec5f067d718d" class="py-8 border rounded-md hover:border-gray-400 group">
+                            <img class="h-12 mx-auto" src="https://paskerid.kemnaker.go.id/storage/173e7033-8219-4ddf-809c-ec5f067d718d_Logo Talenthub.png" alt="siapkerja">
+                            <p class="pt-4 text-center group-hover:text-blue-600">Talenthub</p>
+                        </a>
+                    </div>
+                </div>
             </div>
 
             @include('components.footer')
